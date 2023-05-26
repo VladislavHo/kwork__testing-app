e511320d59ce6e0a900d1646bf8f2f56556b3594
Vladislav <introwlad@gamil.com>
2023-05-26 18:11:04 +0300

    fix: fixed link

--- public_html/js/checkbox/checkbox.js
+++ public_html/js/checkbox/checkbox.js
@@ -4,7 +4,7 @@ const checkbox = document.querySelectorAll('.data__user-checked');
 checkbox.forEach((el, i) =>{
   el.addEventListener('change', function() {
     if (this.checked) {
-      window.open('./policy.html', '_blank');
+      window.open('http://www.consultant.ru/document/cons_doc_LAW_61801', '_blank');
     }
   });
 })
