5fca215f043245d0eca498df29826babf6907906
Vladislav <introwlad@gamil.com>
2023-05-30 23:49:55 +0300

    fix: fixed checkbox click

--- public_html/js/checkbox/checkbox.js
+++ public_html/js/checkbox/checkbox.js
@@ -1,10 +1,11 @@
-const checkbox = document.querySelectorAll('.data__user-checked');
+const checkboxes = document.querySelectorAll('.data__user input[type="checkbox"]');
+const checkboxLabels = document.querySelectorAll('.data__user label');
 
-
-checkbox.forEach((el, i) =>{
-  el.addEventListener('change', function() {
-    if (this.checked) {
+checkboxLabels.forEach((label, index) => {
+  label.addEventListener('click', function() {
+    const i = index
+    if (checkboxes[i].checked) {
       window.open('http://www.consultant.ru/document/cons_doc_LAW_61801', '_blank');
     }
   });
-})
+});
